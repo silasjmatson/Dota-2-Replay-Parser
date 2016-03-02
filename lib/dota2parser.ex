4201844d21add4293f614935fd3c8320073df8d0
Silas J. Matson <silas@clearsightstudio.com>
2016-03-01 21:23:20 -0800

    Fix pattern matching -- spec now passes

--- lib/dota2parser.ex
+++ lib/dota2parser.ex
@@ -15,22 +15,22 @@ defmodule Dota2parser do
     parse(contents)
   end
 
-  defp parse(<<_ignore :: size(24), "\n\bPBDEMS", _ :: size(64), messages :: binary>>) do
+  defp parse(<<_ignore :: size(192), "\n\bPBDEMS", _ :: size(64), messages :: binary>>) do
     parse_message(messages, [])
   end
 
-  defp parse_message(<< command :: size(32),
-                        type :: size(32),
-                        message_length :: size(32),
+  defp parse_message(<< command :: size(8),
+                        type :: size(8),
+                        message_length :: size(8),
                         message :: size(message_length)-unit(8),
                         messages :: binary>>,
                         parsed_messages) do
     # message = %Message{ # parse stuff into it here }
-    # parsed_messages = [message | parsed_messages]
-    # parse_message(messages, parsed_messages)
+    parsed_messages = [message | parsed_messages]
+    parse_message(messages, parsed_messages)
   end
 
-  # defp parse_message(_binary, parsed_messages) do
-  #   parsed_messages
-  # end
+  defp parse_message(_binary, parsed_messages) do
+    parsed_messages
+  end
 end
